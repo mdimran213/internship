c01d124a1497ea86e769eb56a286922300f3a6ed
md imran <md744634@gmail.com>
2023-03-20 17:15:33 +0530

    week7

--- app.py
+++ app.py
@@ -11,13 +11,13 @@ lasso=pickle.load(pickle_in)
 def welcome():
     return "Welcome All"
 
-def Height_and_Weight(Height(Inches)):
+def Height_and_Weight(Height):
     
     """Let's Predict the sales of retail store 
     This is using docstrings for specifications.
     ---
     parameters:  
-      - name: Height(Inches)
+      - name: Height
         in: query
         type: number
         required: true
@@ -27,17 +27,17 @@ def Height_and_Weight(Height(Inches)):
             description: The output values
             
     """
-    prediction=lasso.predict([[Height(Inches)]])
+    prediction=lasso.predict([[Height]])
     print(prediction)
     return prediction
 
 def main():
     st.title("Height and Weight")
-    wHeight(Inches) = st.text_input("Height(Inches)","Type Here")
+    wHeight(Inches) = st.text_input("Height","Type Here")
     
   
     if st.button("Predict"):
-        result=Sales_Of_Retail_Store([Height(Inches)])
+        result=Sales_Of_Retail_Store([Height])
     st.success('The output is {}')
 
 
